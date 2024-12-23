7d756e5b7d6454a69aacdfe2cf65e5f678475171
Dan Barclay <barclaysd@me.com>
2024-12-23 18:36:51 +0000

    feat: fix gradient scroll

--- Packages/Features/Sources/SearchUI/AddFoodView.swift
+++ Packages/Features/Sources/SearchUI/AddFoodView.swift
@@ -18,119 +18,122 @@ public struct AddFoodView: View {
         GeometryReader { geometry in
             ZStack(alignment: .bottom) {
                 ScrollView(showsIndicators: false) {
-                    VStack(spacing: 5) {
-                        Image(systemName: grocerySearchItem.icon).font(.system(size: 78)).foregroundColor(
-                            .white200)
-                        Text("\(grocerySearchItem.name)").font(.largeTitle).lineSpacing(0).foregroundStyle(
-                            .blue800
-                        ).fontWeight(.bold)
-                        HStack {
-                            Text(grocerySearchItem.category)
-                                .font(.callout).foregroundStyle(.gray600)
-                            Circle()
-                                .frame(width: 4, height: 4)
-                                .foregroundStyle(.gray600)
-                            Text("\(String(format: "%.0f", grocerySearchItem.amount)) \(grocerySearchItem.unit)")
-                                .foregroundStyle(.gray600)
-                                .font(.callout)
-                        }
-                        Text(grocerySearchItem.brand)
-                            .font(.headline).fontWeight(.bold)
-                            .foregroundStyle(.brandSainsburys)
+                    ZStack {
+                        LinearGradient(
+                            stops: [
+                                Gradient.Stop(color: .blue700, location: 0),
+                                Gradient.Stop(color: .blue500, location: 0.2),
+                                Gradient.Stop(color: .white200, location: 0.35),
+                            ], startPoint: .top, endPoint: .bottom
+                        )
+                        .ignoresSafeArea(edges: .top)
+                        .offset(y: -geometry.safeAreaInsets.top)
 
-                        VStack {
-                            Text("3%").font(.title).foregroundStyle(.yellow500).fontWeight(.bold).lineSpacing(0)
-                            HStack(spacing: 0) {
-                                Text("Predicted waste score").font(.subheadline).foregroundStyle(.black800)
-                                    .fontWeight(.light)
-                                Image(systemName: "sparkles").font(.system(size: 16)).foregroundColor(.yellow500)
-                                    .offset(x: -2, y: -10)
-                            }.offset(y: -5)
-                        }.padding(.vertical, 10)
-
-                        Grid {
-                            GridRow {
-                                VStack(spacing: 0) {
-                                    Text("32").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
-                                    Text("Previously addded").fontWeight(.light).font(.subheadline).lineLimit(1)
-                                        .foregroundStyle(.blue800)
-                                }
-                                Image(systemName: "calendar.badge.plus")
-                                    .font(.system(size: 32)).fontWeight(.bold)
-                                    .foregroundStyle(.blue800)
-                                VStack(spacing: 0) {
-                                    Text("31").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
-                                    Text("Previously consumed").fontWeight(.light).font(.subheadline).foregroundStyle(
-                                        .blue800)
-                                }
-                            }
-                            GridRow {
-                                VStack(spacing: 0) {
-                                    Text("2").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
-                                        .foregroundStyle(.blue800)
-                                    Text("Located in Fridge").fontWeight(.light).font(.subheadline).lineLimit(1)
-                                        .foregroundStyle(.blue800)
-                                }
-                                Image(systemName: "house")
-                                    .font(.system(size: 32)).fontWeight(.bold)
-                                    .foregroundStyle(.blue800)
-                                VStack(spacing: 0) {
-                                    Text("2").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
-                                    Text("Located in Freezer").fontWeight(.light).font(.subheadline).foregroundStyle(
-                                        .blue800)
-                                }
-                            }
-                        }.padding(.horizontal, 15).padding(.vertical, 5).frame(
-                            maxWidth: .infinity, alignment: .center
-                        ).background(.blue100).cornerRadius(20).padding(.vertical, 10)
-
-                        Grid(horizontalSpacing: 16, verticalSpacing: 20) {
-                            GridRow {
-                                Image(systemName: "checkmark.seal.fill").fontWeight(.bold)
-                                    .foregroundStyle(.yellow500)
-                                    .font(.system(size: 32))
-                                Text("Looks like a good choice, you’re unlikely to waste any of this item")
-                                    .font(.callout)
+                        VStack(spacing: 5) {
+                            Image(systemName: grocerySearchItem.icon).font(.system(size: 78)).foregroundColor(
+                                .white200)
+                            Text("\(grocerySearchItem.name)").font(.largeTitle).lineSpacing(0).foregroundStyle(
+                                .blue800
+                            ).fontWeight(.bold)
+                            HStack {
+                                Text(grocerySearchItem.category)
+                                    .font(.callout).foregroundStyle(.gray600)
+                                Circle()
+                                    .frame(width: 4, height: 4)
                                     .foregroundStyle(.gray600)
-                                    .multilineTextAlignment(.center)
-                                    .lineLimit(2 ... 2)
-
-                                Spacer()
-                            }
-                            GridRow {
-                                Image(systemName: "beach.umbrella.fill")
-                                    .foregroundStyle(.blue600).fontWeight(.bold)
-                                    .font(.system(size: 32))
-                                Text("You should only need to buy one of these before your next holiday")
-                                    .font(.callout)
+                                Text("\(String(format: "%.0f", grocerySearchItem.amount)) \(grocerySearchItem.unit)")
                                     .foregroundStyle(.gray600)
-                                    .multilineTextAlignment(.center)
-                                    .lineLimit(2 ... 2)
-                                Spacer()
+                                    .font(.callout)
                             }
+                            Text(grocerySearchItem.brand)
+                                .font(.headline).fontWeight(.bold)
+                                .foregroundStyle(.brandSainsburys)
 
-                        }.padding(.vertical, 5).padding(.horizontal, 20)
+                            VStack {
+                                Text("3%").font(.title).foregroundStyle(.yellow500).fontWeight(.bold).lineSpacing(0)
+                                HStack(spacing: 0) {
+                                    Text("Predicted waste score").font(.subheadline).foregroundStyle(.black800)
+                                        .fontWeight(.light)
+                                    Image(systemName: "sparkles").font(.system(size: 16)).foregroundColor(.yellow500)
+                                        .offset(x: -2, y: -10)
+                                }.offset(y: -5)
+                            }.padding(.vertical, 10)
 
-                        VStack(spacing: 10) {
-                            ConsumableCategory(isExpiryDateToggled: $isExpiryDateSet)
-                            ConsumableCategory(isExpiryDateToggled: $isStorageSet)
-                            ConsumableCategory(isExpiryDateToggled: $isOpenedSet)
-                            ConsumableCategory(isExpiryDateToggled: $isQuantitySet)
+                            Grid {
+                                GridRow {
+                                    VStack(spacing: 0) {
+                                        Text("32").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
+                                        Text("Previously addded").fontWeight(.light).font(.subheadline).lineLimit(1)
+                                            .foregroundStyle(.blue800)
+                                    }
+                                    Image(systemName: "calendar.badge.plus")
+                                        .font(.system(size: 32)).fontWeight(.bold)
+                                        .foregroundStyle(.blue800)
+                                    VStack(spacing: 0) {
+                                        Text("31").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
+                                        Text("Previously consumed").fontWeight(.light).font(.subheadline).foregroundStyle(
+                                            .blue800)
+                                    }
+                                }
+                                GridRow {
+                                    VStack(spacing: 0) {
+                                        Text("2").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
+                                            .foregroundStyle(.blue800)
+                                        Text("Located in Fridge").fontWeight(.light).font(.subheadline).lineLimit(1)
+                                            .foregroundStyle(.blue800)
+                                    }
+                                    Image(systemName: "house")
+                                        .font(.system(size: 32)).fontWeight(.bold)
+                                        .foregroundStyle(.blue800)
+                                    VStack(spacing: 0) {
+                                        Text("2").fontWeight(.bold).font(.headline).foregroundStyle(.blue800)
+                                        Text("Located in Freezer").fontWeight(.light).font(.subheadline).foregroundStyle(
+                                            .blue800)
+                                    }
+                                }
+                            }.padding(.horizontal, 15).padding(.vertical, 5).frame(
+                                maxWidth: .infinity, alignment: .center
+                            ).background(.blue100).cornerRadius(20).padding(.vertical, 10)
+
+                            Grid(horizontalSpacing: 16, verticalSpacing: 20) {
+                                GridRow {
+                                    Image(systemName: "checkmark.seal.fill").fontWeight(.bold)
+                                        .foregroundStyle(.yellow500)
+                                        .font(.system(size: 32))
+                                    Text("Looks like a good choice, you’re unlikely to waste any of this item")
+                                        .font(.callout)
+                                        .foregroundStyle(.gray600)
+                                        .multilineTextAlignment(.center)
+                                        .lineLimit(2 ... 2)
+
+                                    Spacer()
+                                }
+                                GridRow {
+                                    Image(systemName: "beach.umbrella.fill")
+                                        .foregroundStyle(.blue600).fontWeight(.bold)
+                                        .font(.system(size: 32))
+                                    Text("You should only need to buy one of these before your next holiday")
+                                        .font(.callout)
+                                        .foregroundStyle(.gray600)
+                                        .multilineTextAlignment(.center)
+                                        .lineLimit(2 ... 2)
+                                    Spacer()
+                                }
+
+                            }.padding(.vertical, 5).padding(.horizontal, 20)
+
+                            VStack(spacing: 10) {
+                                ConsumableCategory(isExpiryDateToggled: $isExpiryDateSet)
+                                ConsumableCategory(isExpiryDateToggled: $isStorageSet)
+                                ConsumableCategory(isExpiryDateToggled: $isOpenedSet)
+                                ConsumableCategory(isExpiryDateToggled: $isQuantitySet)
+                            }
                         }
+                        .padding(.bottom, 100)
+                        .padding(.horizontal, 20)
+                        .frame(maxWidth: .infinity)
                     }
-                    .padding(.bottom, 100)
                 }
-                .padding(.horizontal, 20)
-                .frame(maxWidth: .infinity)
-                .background(
-                    LinearGradient(
-                        stops: [
-                            Gradient.Stop(color: .blue700, location: -0.05),
-                            Gradient.Stop(color: .blue500, location: 0.10),
-                            Gradient.Stop(color: .white200, location: 0.22),
-                        ], startPoint: .top, endPoint: .bottom
-                    )
-                )
 
                 ZStack {
                     UnevenRoundedRectangle(
