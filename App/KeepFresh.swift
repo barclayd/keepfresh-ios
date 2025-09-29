cfab2ab31cf4caa4c48017138db32ec1760b32c0
Dan Barclay <barclaysd@me.com>
2025-09-29 22:58:18 +0100

    revert: foundation model changes

--- App/KeepFresh.swift
+++ App/KeepFresh.swift
@@ -2,16 +2,15 @@ import DesignSystem
 import Environment
 import Router
 import SwiftUI
-import FoundationModels
 
 public class FontRegistration {
     public static func registerFonts() {
         let bundle = Bundle(for: FontRegistration.self)
-
+        
         guard let bundleURL = bundle.url(forResource: "Shrikhand-Regular", withExtension: "ttf") else {
             return
         }
-
+        
         CTFontManagerRegisterFontsForURL(bundleURL as CFURL, .process, nil)
     }
 }
@@ -20,11 +19,11 @@ public class FontRegistration {
 struct KeepFreshApp: App {
     @State var router: Router = .init()
     @State var inventory: Inventory = .init()
-
+    
     init() {
         FontRegistration.registerFonts()
     }
-
+    
     var body: some Scene {
         WindowGroup {
             AppTabRootView()
@@ -32,24 +31,6 @@ struct KeepFreshApp: App {
                 .environment(inventory)
                 .task {
                     await inventory.fetchItems()
-                }
-                .onAppear {
-                    Task {
-                        let session = LanguageModelSession(instructions: """
-                            You are an expert in predicting food wastage. For each set of data, calculate and return only the most likely waste percentage for the user's next purchase of a given food item.
-                                Consumed - means 0% food waste for a given food waste
-                            """)
-                        let response = try await session.respond(to: """
-                            Food Name: Chicken Thighs
-                            Purchased Count: 8
-                            Consumed Count: 4
-                            Discard Count: 4
-                            Average percentage wasted of product when discarded: 25%
-                            """)
-                        
-                        print("response: \(response)")
-                    }
-                    
                 }
         }
     }
