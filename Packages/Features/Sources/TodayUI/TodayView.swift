c2fc20539d5e78d4374cfe217525f7bea10a6615
Dan Barclay <barclaysd@me.com>
2025-10-05 14:37:14 +0100

    feat: empty state today view

--- Packages/Features/Sources/TodayUI/TodayView.swift
+++ Packages/Features/Sources/TodayUI/TodayView.swift
@@ -6,15 +6,15 @@ import SwiftUI
 
 public struct TodayView: View {
     public init() {}
-
+    
     @Environment(\.dynamicTypeSize) private var dynamicTypeSize
     @Environment(Inventory.self) var inventory
-
+    
     private func getSheetFraction(height: CGFloat) -> CGFloat {
         if dynamicTypeSize >= .xxLarge {
             return 0.8
         }
-
+        
         switch height {
         case ..<668:
             return 1 // iPhone SE
@@ -26,19 +26,69 @@ public struct TodayView: View {
             return 0.7
         }
     }
-
+    
     public var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 14) {
-                ForEach(inventory.itemsSortedByExpiryAscending) { inventoryItem in
-                    InventoryItemView(inventoryItem: inventoryItem)
+        if inventory.items.isEmpty {
+            VStack(spacing: 8) {
+                HStack {
+                    Spacer()
+                    Image("arrow.curved.right")
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 300)
+                        .foregroundStyle(.green500)
+                }.padding(.horizontal, 40)
+                
+                Text("Bring your fridge to your pocket").font(.headline).foregroundStyle(
+                    .blue600
+                ).fontWeight(.bold)
+                Text("Tap above to search for or scan a grocery item from the UK’s favourite supermarkets").font(.subheadline).foregroundStyle(
+                    .blue800
+                ).multilineTextAlignment(.center).padding(.horizontal, 20)
+                
+                Spacer()
+                
+                Text("Need some inspiraton?").font(.subheadline).foregroundStyle(
+                    .blue600
+                ).fontWeight(.bold)
+                
+                Button(action: {
+                    print("randomise")
+                }) {
+                    HStack(spacing: 10) {
+                        Image(systemName: "dice")
+                            .font(.system(size: 18))
+                            .frame(width: 20, alignment: .center)
+                        Text("Random item")
+                            .font(.headline)
+                            .frame(width: 175, alignment: .center)
+                    }
+                    .foregroundStyle(.blue600)
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.gray200))
                 }
             }
             .padding(.horizontal, 20)
-            .padding(.top, 20)
-            .padding(.bottom, 10)
-            .redactedShimmer(when: inventory.state == .loading)
+            .padding(.bottom, 20)
+            .background(.white200)
+        } else {
+            ScrollView {
+                LazyVStack(spacing: 14) {
+                    ForEach(inventory.itemsSortedByExpiryAscending) { inventoryItem in
+                        InventoryItemView(inventoryItem: inventoryItem)
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+                .padding(.bottom, 10)
+                .redactedShimmer(when: inventory.state == .loading)
+            }
+            .background(.white200)
         }
-        .background(.white200)
     }
 }
