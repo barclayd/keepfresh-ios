eac3916e727dbfa93b9ea100ea315837cd37a9e1
Dan Barclay <barclaysd@me.com>
2025-11-15 14:35:56 +0000

    chore: rearrange list

--- Packages/Features/Sources/ShoppingUI/StorageLocationPanel.swift
+++ Packages/Features/Sources/ShoppingUI/StorageLocationPanel.swift
@@ -5,44 +5,34 @@ import SwiftUI
 
 public struct StorageLocationPanel: View {
     @State private var isToggled: Bool = false
-    
+
+    @State private var shoppingListItems: [ShoppingListItem] = [
+        ShoppingListItem(id: 1, createdAt: Date(), updatedAt: Date(), source: .userAdded, status: .added, storageLocation: .fridge, product: Product(id: 1, name: "Semi Skimmed Milk", unit: "pts", brand: .tesco, amount: 4, category: CategoryDetails(icon: "milk", id: 1, name: "Milk", pathDisplay: "Fresh Food > Dairy > Milk"))),
+        ShoppingListItem(id: 2, createdAt: Date(), updatedAt: Date(), source: .userAdded, status: .added, storageLocation: .fridge, product: Product(id: 1, name: "Whole Milk", unit: "pts", brand: .tesco, amount: 4, category: CategoryDetails(icon: "milk", id: 1, name: "Milk", pathDisplay: "Fresh Food > Dairy > Milk"))),
+        ShoppingListItem(id: 3, createdAt: Date(), updatedAt: Date(), source: .userAdded, status: .added, storageLocation: .fridge, product: Product(id: 1, name: "Skimmed Milk", unit: "pts", brand: .tesco, amount: 4, category: CategoryDetails(icon: "milk", id: 1, name: "Milk", pathDisplay: "Fresh Food > Dairy > Milk"))),
+    ]
+
     let storageLocation: StorageLocation
-    
+
     public init(storageLocation: StorageLocation) {
         self.storageLocation = storageLocation
     }
-    
+
     var textColor: Color {
         storageLocation == .freezer ? .white200 : .blue800
     }
-    
-//    public var body: some View {
-//        List {
-//            ForEach(0 ..< 3, id: \.self) { _ in
-//                ShoppingListItemView(shoppingListItem: ShoppingListItem(id: 1, createdAt: Date(), updatedAt: Date(), source: .userAdded, status: .added, storageLocation: .fridge, product: Product(id: 1, name: "Semi Skimmed Milk", unit: "pts", brand: .tesco, amount: 4, category: CategoryDetails(icon: "milk", id: 1, name: "Milk", pathDisplay: "Fresh Food > Dairy > Milk"))))
-//            }
-//            .listRowSeparator(.hidden)
-//            .listRowBackground(Color.clear)
-//        }
-//        .listStyle(.plain)
-//        .frame(height: 650)
-//        .padding(.vertical, 10).padding(.horizontal, 15).frame(maxWidth: .infinity)
-//            .background(
-//                UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(
-//                    topLeading: 0,
-//                    bottomLeading: 10,
-//                    bottomTrailing: 10,
-//                    topTrailing: 0))
-//                    .fill(LinearGradient(stops: storageLocation.viewGradientStopsReversed, startPoint: .leading, endPoint: .trailing)))
-//    }
-    
+
+    func move(indexSet: IndexSet, int: Int) {
+        shoppingListItems.move(fromOffsets: indexSet, toOffset: int)
+    }
+
     public var body: some View {
         VStack(spacing: 0) {
             HStack {
                 HStack(alignment: .firstTextBaseline) {
                     Image(systemName: storageLocation.iconFilled)
                         .frame(width: 18).foregroundColor(textColor).fontWeight(.bold)
-                    
+
                     Text(storageLocation.rawValue.capitalized)
                         .fontWeight(.bold)
                         .foregroundStyle(textColor)
@@ -52,9 +42,9 @@ public struct StorageLocationPanel: View {
                             d[.bottom] * 0.75
                         }
                 }
-                
+
                 Spacer()
-                
+
                 HStack {
                     Image(systemName: "5.square.fill")
                         .frame(width: 18).foregroundColor(textColor)
@@ -83,8 +73,8 @@ public struct StorageLocationPanel: View {
                     RoundedRectangle(cornerRadius: 10).fill(Color.black).opacity(0.15).frame(maxWidth: .infinity, maxHeight: 1).offset(y: -10)
 
                     List {
-                        ForEach(0 ..< 3, id: \.self) { _ in
-                            ShoppingListItemView(shoppingListItem: ShoppingListItem(id: 1, createdAt: Date(), updatedAt: Date(), source: .userAdded, status: .added, storageLocation: .fridge, product: Product(id: 1, name: "Semi Skimmed Milk", unit: "pts", brand: .tesco, amount: 4, category: CategoryDetails(icon: "milk", id: 1, name: "Milk", pathDisplay: "Fresh Food > Dairy > Milk"))))
+                        ForEach(shoppingListItems, id: \.self) { shoppingListItem in
+                            ShoppingListItemView(shoppingListItem: shoppingListItem)
                                 .swipeActions(edge: .trailing, allowsFullSwipe: true) {
                                     Button(role: .destructive) {
 //                                        store.delete(message)
@@ -98,6 +88,7 @@ public struct StorageLocationPanel: View {
                                     }
                                 }
                         }
+                        .onMove(perform: move)
                         .listRowInsets(EdgeInsets())
                         .listRowSeparator(.hidden)
                         .listRowBackground(Color.clear)
