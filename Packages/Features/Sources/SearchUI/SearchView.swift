f30ff38ef32c2c9ba221f5a89e93a7b52dc69836
Dan Barclay <barclaysd@me.com>
2025-11-30 18:19:07 +0000

    chore: remove unused comments

--- Packages/Features/Sources/SearchUI/SearchView.swift
+++ Packages/Features/Sources/SearchUI/SearchView.swift
@@ -120,7 +120,6 @@ class Search {
         do {
             let searchResponse = try await api.searchProducts(query: lastSearchedTerm, page: currentPage)
 
-            // Filter out duplicates before appending
             let existingIds = Set(searchResults.map { $0.id })
             let newResults = searchResponse.results.filter { !existingIds.contains($0.id) }
 
